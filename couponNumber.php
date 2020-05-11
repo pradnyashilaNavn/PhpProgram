f16dbeb9798ca81d0d069a79bbe03b61529ce158
pradnyashilaNavn <pradnyajadhav995@gmail.com>
2020-05-12 02:52:59 +0530

    [pradnya]Add. coupon program

---
+++ couponNumber.php
@@ -0,0 +1,19 @@
+<?php
+/********************************************************************
+* @Execution : Functional/MainPrograms $ php couponNumber.php
+* @Description :Given N distinct Coupon Numbers, how many random numbers do you
+* need to generate distinct coupon number? This program simulates this random
+* process.
+* @Purpose : Total random number needed to have all distinct numbers.
+* @Function : Utility/utility
+* @File : couponNumber.php
+* @Author : Pradnyashila Jadhav 
+* <pradnyajadhav995@gmail.com>
+* @version : 7.2.14
+* @Since : 06-05-2020
+*********************************************************************/
+include "C:\Users\pc\Function\Functional\Utility\Utility.php";
+echo "Enter coupoun numbers you want: ";
+$number=readline();
+Utility::couponNumber($number);
+
