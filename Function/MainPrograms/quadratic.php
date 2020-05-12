dcc0ff9be8aff2af73ec88e4ebc8e55bef7dbe88
pradnyashilaNavn <pradnyajadhav995@gmail.com>
2020-05-12 11:05:51 +0530

    [pradnya]Refactor. an added input validation

---
+++ Function/MainPrograms/quadratic.php
@@ -0,0 +1,21 @@
+<?php
+/********************************************************************
+* @Execution : Functional/MainPrograms $ php quadratic.php
+* @Description :Find the roots of the equation a*x*x + b*x + c.Since the equation is x*x, hence there are 2  roots.
+* @Purpose : Find Quadratic equation.
+* @Function : Utility/utility
+* @File : quadratic.php
+* @Author : Pradnyashila Jadhav 
+* <pradnyajadhav995@gmail.com>
+* @version : 7.2.14
+* @Since : 06-05-2020
+*********************************************************************/
+include "C:\Users\pc\PHP\Function\Utility\utility.php"; 
+echo "enter a value: ";
+$a= Utility::getInt();
+echo "\nenter b value: ";
+$b= Utility::getInt();
+echo "\nenter c vale: ";
+$c= Utility::getInt();
+Utility::quadratic($a,$b,$c);
+?>
