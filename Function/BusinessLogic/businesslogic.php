2dc532e32b40460468b4c92ec122e9d535c5f43a
pradnyashilaNavn <pradnyajadhav995@gmail.com>
2020-05-12 11:43:28 +0530

    [pradnya]Add. Added simple sumofThree program

--- Function/BusinessLogic/businesslogic.php
+++ Function/BusinessLogic/businesslogic.php
@@ -19,4 +19,28 @@ class BusinessLogic{
              echo "invalid input"."\n";
          }                                
     }
+     /*
+    *@description : Finding sum of three numbers are zeros
+    *$parameter : Reads the input integers  from user
+    *@return : sum of three equals to zero.
+    */
+    public function sumofThree($arr, $number)
+    {
+        $count = 0;
+        for($i = 0; $i < $number-2; $i++)
+        {
+            for($j = $i + 1; $j <  $number-1 ; $j++)
+            {
+                for($k = $j + 1; $k < $number; $k++)
+                {
+                    if($arr[$i]+$arr[$j]+$arr[$k]==0)
+                    {
+                        echo $arr[$i]." + ".$arr[$j]." + ".$arr[$k]." = 0"."\n";
+                        $count++;
+                    }
+                }
+            }
+        }
+        echo "total sum ".$count;
+    }
 }
---
+++ Function/MainPrograms/sumofThree.php
@@ -0,0 +1,25 @@
+<?php
+/********************************************************************
+* @Execution : Functional/MainPrograms $ php sumofThree.php
+* @description :A program with cubic running time. Read in N integers and counts the
+    number of triples that sum to exactly 0.
+* @Purpose : Finding sum of three are zero
+* @Function : Utility/utility
+* @File : sumofThree.php
+* @Author : Pradnyashila Jadhav 
+* <pradnyajadhav995@gmail.com>
+* @version : 7.2.14
+* @Since : 06-05-2020
+*********************************************************************/
+include "C:\Users\pc\PHP\Function\Utility\utility.php"; 
+include "C:\Users\pc\PHP\Function\BusinessLogic\businesslogic.php";
+echo "Enter array size : "."\n";
+$number =Utility::getInt();
+$arr = array();           
+echo "\n"."Enter values"."\n";
+    for($i=0; $i<$number; $i++)
+{
+    $arr[$i] = readline();
+}
+BusinessLogic::sumofThree($arr, $number);
+?>
