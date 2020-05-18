3999918e41f47f3774e94bc235a546be94b8f904
pradnyashilaNavn <pradnyajadhav995@gmail.com>
2020-05-18 23:47:08 +0530

    [pradnya]Refactor. printing values of 2Darray

--- DataStructure/MainPrograms/prime2Darray.php
+++ DataStructure/MainPrograms/prime2Darray.php
@@ -19,11 +19,11 @@ $index = 0;
 $range = 100;
 for ($i = 0; $i < 10; $i++) {
     for ($j = 0; $j < 100; $j++) {
-        /**
-         * if index reach size of array then break or when the value of index
+        /*
+         *if index reach size of array then break or when the value of index
          *elemnet is greater then $n value break
          */
-        if ($index == sizeof($array) || $array[$index] > $range)
+        if ($index == count($array) || $array[$index] > $range)
             break;
         $input[$i][$j] = $array[$index++];
     }
@@ -31,7 +31,11 @@ for ($i = 0; $i < 10; $i++) {
      * after every loop increase by 100
      */
     $range += 100;
-      array_push($input,$array);
-      echo "\n";
-     }
-     print_r($input);
+}
+    for ($i = 0; $i < count($input); $i++) {
+        for ($j = 0; $j < count($input[$i]); $j++) {
+            echo $input[$i][$j] . " ";
+        }
+        echo "\n";
+    
+    }
